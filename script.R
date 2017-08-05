19ffa37352d7c0090af488b8e6706849fd89e7a4
ShikharParashar <shikhar.parashar@gmail.com>
2017-08-05 10:09:18 +0530

    Update script.R

--- script.R
+++ script.R
@@ -1,1 +1,26 @@
+rm(list=ls())
+
+setwd("../Titanic/")
+
+get_data <- function(){
+  
+  data_train <- read.csv(textConnection("https://www.kaggle.com/c/titanic/download/test.csv"), stringsAsFactors=FALSE, header=TRUE)
+  data_test <- read.csv(textConnection("https://www.kaggle.com/c/titanic/download/train.csv"), stringsAsFactors=FALSE, header=TRUE)
+  
+  data_train$isTrainingSet <- TRUE
+  data_test$isTrainingSet <- FALSE
+  
+  data_test$Survived <- NA
+  
+  data_all <- dplyr::bind_rows(data_train, data_test)
+  
+  return(data_all)
+ 
+  }
+  
+
+
+
+main
+
 
